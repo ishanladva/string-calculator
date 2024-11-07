e845de7905f1909d44dcbdc0512d666af4be3d26
ishan.ladva <ishan.ladva@openxcell.com>
2024-11-07 23:26:01 +0530

    completed complex test case and implement features into method of StringCalculator class

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,39 @@ export class StringCalculator {
       return 0;
     }
 
-    const numberArray = input.split(/[\n,]/).map(Number);
+    let delimiters = /,|\n/;
 
-    return numberArray.reduce((sum, num) => sum + num, 0);
+    if (input.startsWith("//")) {
+      const delimiterPartEnd = input.indexOf("\n");
+      const delimiterPart = input.slice(2, delimiterPartEnd);
+      input = input.slice(delimiterPartEnd + 1);
+
+      const customDelimiters = delimiterPart.match(/\[(.*?)\]/g);
+      if (customDelimiters) {
+        delimiters = new RegExp(
+          customDelimiters
+            .map((d) =>
+              d.slice(1, -1).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+            )
+            .join("|")
+        );
+      } else {
+        delimiters = new RegExp(
+          delimiterPart.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+        );
+      }
+    }
+
+    const numbers = input.split(delimiters).map((num) => parseInt(num));
+
+    const negatives = numbers.filter((num) => num < 0);
+    if (negatives.length) {
+      throw new Error(
+        `Negative numbers are not allowed: ${negatives.join(", ")}`
+      );
+    }
+    return numbers
+      .filter((num) => num <= 1000)
+      .reduce((sum, acc) => sum + acc, 0);
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -23,4 +23,32 @@ describe("StringCalculator", () => {
   it("should handle new lines as delimiters", () => {
     expect(calculator.calculate("1\n2,3")).to.equal(6);
   });
+
+  it("should support custom delimiters", () => {
+    expect(calculator.calculate("//;\n1;2;3")).to.equal(6);
+  });
+
+  it("should throw an error when a negative number is provided", () => {
+    expect(() => calculator.calculate("1,-2,3")).to.throw(
+      "Negative numbers are not allowed: -2"
+    );
+  });
+
+  it("should throw an error listing all negative numbers when multiple negatives are provided", () => {
+    expect(() => calculator.calculate("1,-2,-3,4")).to.throw(
+      "Negative numbers are not allowed: -2, -3"
+    );
+  });
+
+  it("should handle multiple custom delimiters", () => {
+    expect(calculator.calculate("//[*][%]\n1*2%3")).to.equal(6);
+  });
+
+  it("should handle multiple complex custom delimiters", () => {
+    expect(calculator.calculate("//[***][%%%]\n1***2%%%3")).to.equal(6);
+  });
+
+  it("Numbers bigger than 1000 should be ignored", () => {
+    expect(calculator.calculate("//;\n1;1001;3")).to.equal(4);
+  });
 });
